58eb8bde3e93977d3bab24bba24e44ee6c97d742
MatyldeIgier <matyldeigier@gmail.com>
2015-05-27 09:29:33 +0200

    log

--- WifiP2P/app/src/main/java/com/example/rafaelle/wifip2p/WifiP2PActivity.java
+++ WifiP2P/app/src/main/java/com/example/rafaelle/wifip2p/WifiP2PActivity.java
@@ -8,8 +8,11 @@ import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.view.View;
+import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 import java.nio.channels.Channel;
 
@@ -21,11 +24,11 @@ public class WifiP2PActivity extends ActionBarActivity {
     Context context;
     IntentFilter mIntentFilter = new IntentFilter();
 
-
+//je pense qu'il faut faire un new wifiConnection
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.v("Dans on create", "on rentre bien dans OnCreate");
         setContentView(R.layout.activity_wifi_p2_p);
         context = getApplicationContext();
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
@@ -36,13 +39,29 @@ public class WifiP2PActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.v("Dans onResume", "on rentre bien dans OnResume");
         registerReceiver(mReceiver, mIntentFilter);
 }
     /* unregister the broadcast receiver */
     @Override
     protected void onPause() {
         super.onPause();
+        Log.v("Dans onPause", "on rentre bien dans OnPause");
         unregisterReceiver(mReceiver);
+    }
+
+    public void startScan(View v){
+        if(((Button)findViewById(R.id.bouton)).getText().equals("Start Scanning")){
+            WifiConnection.startDiscovery();
+            ((Button)findViewById(R.id.bouton)).setText("Stop Scanning");
+        }else{
+            WifiConnection.stopDiscovery();
+            ((Button)findViewById(R.id.bouton)).setText("Start Scanning");
+        }
+    }
+
+    public void closeConnections(View v){
+        WifiConnection.closeConnections();
     }
 
 
--- WifiP2P/app/src/main/java/com/example/rafaelle/wifip2p/WifiP2Pconnection.java
+++ WifiP2P/app/src/main/java/com/example/rafaelle/wifip2p/WifiP2Pconnection.java
@@ -8,9 +8,12 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pGroup;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.app.AlertDialog;
 import android.os.Looper;
+import android.util.Log;
 
 import java.nio.channels.Channel;
+import java.util.Iterator;
 
 /**
  * Created by Rafaelle on 21/05/2015.
@@ -22,6 +25,9 @@ public class WifiP2Pconnection extends BroadcastReceiver implements  WifiP2pMana
     private WifiP2pManager mManager;
     private Channel mChannel; //on suppose que le channel est la connection entre 2 appareils
     private WifiP2PActivity mActivity;
+    private Context ctx;
+    private Boolean discoveryOn=false;
+    AlertDialog.Builder adbldr;
     private WifiP2pManager.PeerListListener myPeerListListener;
     private IntentFilter mIntentFilter = new IntentFilter();
 
@@ -29,9 +35,12 @@ public class WifiP2Pconnection extends BroadcastReceiver implements  WifiP2pMana
     public WifiP2Pconnection(Context ctxt, WifiP2pManager manager, Looper looper,
                              WifiP2PActivity activity) {
         super();
+        Log.v("Dans WifiP2pCo", "on rentre bien dans WifiP2PCo");
+        ctx=ctxt;
+
         this.mManager = manager;
         this.mActivity = activity; //pour relier � l'activit� principale
-
+        adbldr = new AlertDialog.Builder(ctx);
         //mManager = (WifiP2pManager) activity.getSystemService(Context.WIFI_P2P_SERVICE);
         //j'appelle directement cette m�thode dans activity
         this.mChannel = (Channel) mManager.initialize(ctxt, looper, null);
@@ -48,20 +57,30 @@ public class WifiP2Pconnection extends BroadcastReceiver implements  WifiP2pMana
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
 
-
+        Log.v("Dans onreceive", "on rentre bien dans OnReceive");
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            Log.v("Dans onReceive", "l'etat de la wifi est changé");
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Log.v("Dans onReceive", "l'etat de la wifi est ok");
+
                 // Wifi P2P is enabled
-            } else {
+            } else {            Log.v("Dans onReceive", "l'etat de la wifi est pas ok");
+
                 // Wi-Fi P2P is not enabled
             }
             // Check to see if Wi-Fi is enabled and notify appropriate activity
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            Log.v("Dans onReceive", "on a de nouveau pairs à rechercher");
+
             // Call WifiP2pManager.requestPeers() to get a list of current peers
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            Log.v("Dans onReceive", "repondre à une nouvelle co ou se deco");
+
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            Log.v("Dans onReceive", "dire que notre etat de co change");
+
             // Respond to this device's wifi state changing
         }
 
@@ -71,6 +90,8 @@ public class WifiP2Pconnection extends BroadcastReceiver implements  WifiP2pMana
             // asynchronous call and the calling activity is notified with a
             // callback on PeerListListener.onPeersAvailable()
             if (mManager != null) {
+                Log.v("Dans onReceive", "on cherche de nouveau pairs");
+
                 mManager.requestPeers((WifiP2pManager.Channel) mChannel, myPeerListListener);
             }
         }
@@ -81,6 +102,27 @@ public class WifiP2Pconnection extends BroadcastReceiver implements  WifiP2pMana
     public void discoverPeers(){
         mManager.discoverPeers((WifiP2pManager.Channel) mChannel, this);
     }
+
+    //allow manager to discover peers and connect to other devices
+    public void startDiscovery(){
+        discoveryOn = true;
+        Intent i = new Intent("scanAlarm");
+        ctx.sendBroadcast(i);
+    }
+
+    public void closeConnections(){
+        mManager.removeGroup((WifiP2pManager.Channel) mChannel, this);
+        //TODO: ici j'ai remplacé connection par notre classe c'est possible que je me soit trompé !sorry
+      //  Iterator<WifiP2Pconnection> it = connections.iterator();
+      //  while(it.hasNext()){
+      //      P2PConnection con = it.next();
+      //      con.disconnect();
+       // }
+       // adapter.notifyDataSetChanged();
+    }
+
+    //stop trying to connect to other devices
+    public void stopDiscovery(){ discoveryOn = false; }
 
     @Override
     public void onSuccess() {

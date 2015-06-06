3444de8fc8b9ddc7d245f84b8823ffa650ca4a70
rafikoi <rafa.botter@gmail.com>
2015-06-06 18:25:58 +0200

    le send

--- app/src/main/java/com/mathildeprojet/mathtest/Sender.java
+++ app/src/main/java/com/mathildeprojet/mathtest/Sender.java
@@ -10,6 +10,7 @@ import android.util.Log;
 
 import com.mathildeprojet.mathtest.WifiP2Pconnection;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -30,11 +31,12 @@ public class Sender {
         socket=new DatagramSocket();
     }
 
-    public void send() throws UnknownHostException {
+    public void send() throws IOException {
         InetAddress adr = InetAddress.getByName("224.0.0.1");
         InetAddress moi= socket.getLocalAddress();
         byte[] data = new byte[message.length()];
         Log.v("Nous", "mon message "+message+ " adresse multicast "+adr + " mon adresse a moi " + moi);
+        socket.send(new DatagramPacket(data,message.length(),adr,8888));
     }
 
 
